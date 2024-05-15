5d92840b06b47506fd447b3faa40419b8ca7cfa5
mateus-hub <theusskt2018@gmail.com>
2024-05-15 16:54:22 -0300

    criando o repository com spring data

--- src/main/java/br/com/springboot/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.model.Usuario;
+import br.com.springboot.repository.UsuarioRepository;
+
 @RestController
 public class GreetingsController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
@@ -19,6 +26,11 @@ public class GreetingsController {
 	@RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public String retornaOlaMundo(@PathVariable String nome) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+		usuarioRepository.save(usuario);
+		
 		return "Ola mundo " + nome;
 
 	}
